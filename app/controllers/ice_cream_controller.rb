ef1f2bf38ec1327c44818260e28c3dc779923af6
JaSON <masteraccounthub@gmail.com>
2020-12-01 17:34:00 -0500

    finished pod group

--- app/controllers/ice_cream_controller.rb
+++ app/controllers/ice_cream_controller.rb
@@ -1,20 +1,20 @@
 class IceCreamController < ApplicationController
     get '/ice_creams' do 
-        "All Icecreams"
-
+        erb :"ice_creams/index"
     end
     
     get '/ice_creams/new' do
-        "New Form"
+        erb :"ice_creams/new"
     end
 
     post '/ice_creams' do
+        binding.pry
         "Create"
     end
 
     get '/ice_creams/:id' do
-        "Show page"
+     
+        erb :"ice_creams/show"
     end
 
-    
 end
